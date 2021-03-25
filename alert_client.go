ad8652968c47905949ae1a0481ae74edc1c8fdec
Bamboer <57797847+Bamboer@users.noreply.github.com>
2021-03-25 13:43:30 +0800

    Create alert_client.go

---
+++ alert_client.go
@@ -0,0 +1,43 @@
+package main
+import (
+  "os"
+  "fmt"
+  "time"
+  "path/filepath"
+  "grafana/pkg/configer"
+  "grafana/pkg/client"
+  "grafana/pkg/log"
+  "grafana/pkg/notification"
+)
+
+var (
+  ConfigFile string
+  Version   bool
+)
+var (
+  Version = "v1"
+  DEFALTCONF = "alert_client.conf"
+)
+
+func init(){
+  ConfigFile := flag.String("config","alert_client.conf","alert client configuration file.")
+  Version := flag.Bool("version",false,"Print version.")
+
+  if os.Args[1] == "versions"{
+    fmt.Println("grafana alert client ",Version)
+    os.Exit(0)
+  }
+}
+
+func run(){
+  ticker := time.NewTicker(30 * time.Second)
+  for _ = range ticker.C{
+
+  }
+}
+
+func main(){
+  flag.Parse()
+  client,_ := client.NewGrafanaClient("http://10.40.43.170:3000","eyJrIjoia1AyeWhsbUpyMTE2VU41TlBCRnZHc0VNOFoyM1FEcWIiLCJuIjoidGVzdDEiLCJpZCI6MX0=")
+  fmt.Println(data)
+}
