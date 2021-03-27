46909c0f16f57e51e180ad65edb4db4fa5e87a38
Bamboer <Bamboer@git.com>
2021-03-28 03:28:12 +0800

    update info.go client.go

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,65 +1,87 @@
-package main
+package client
+
 import (
-  "fmt"
-  "encoding/json"
-  "net/http"
-  "net/url"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"reflect"
 )
 
-
-var  gr interface{}
-
 type grafana_client struct {
-  uri  *url.URL
-  token string
-  httpClient *http.Client
+	uri        *url.URL
+	token      string
+	httpClient *http.Client
 }
 
-func NewGrafanaClient(uri,token string)(*grafana_client,error){
-  url,err := url.Parse(uri)
-  token = "Bearer " + token
-  if err != nil{
-    return nil,err
-  }
-  return &grafana_client{
-    uri: url,
-    token: token,
-    httpClient: &http.Client{},
-  },nil
+func NewGrafanaClient(uri, token string) (*grafana_client, error) {
+	url, err := url.Parse(uri)
+	token = "Bearer " + token
+	if err != nil {
+		return nil, err
+	}
+	return &grafana_client{
+		uri:        url,
+		token:      token,
+		httpClient: &http.Client{},
+	}, nil
 }
 
-func (c *grafana_client) Get(path string) (*interface{}, error) {
-        uri := c.uri
-        uri.Path = path
-        req, err := http.NewRequest("GET", uri.String(), nil)
-        if err != nil {
-                return nil, err
-        }
-        req.Header.Add("Authorization", c.token)
-//        req.SetBasicAuth("admin", "admin")
-        req.Header.Add("Content-Type", "application/json")
-        req.Header.Add("Accept", "application/json")
-        resp, err := c.httpClient.Do(req)
-//        fmt.Println("Head: ",req)
-        if err != nil {
-                return nil, err
-        }
-        defer resp.Body.Close()
-        err = json.NewDecoder(resp.Body).Decode(&gr)
- //       fmt.Println(resp)
-        if err != nil {
-                return nil, err
-        }
-        return &gr, nil
+func (c *grafana_client) Get(path string) (io.ReadCloser, error) {
+	uri := c.uri
+	uri.Path = path
+	req, err := http.NewRequest("GET", uri.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", c.token)
+	//        req.SetBasicAuth("admin", "admin")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	//        err = json.NewDecoder(resp.Body).Decode(&gr)
+	//        if err != nil {
+	//                return nil, err
+	//        }
+	return resp.Body, nil
 }
 
+/*
 func main(){
-  C,_ := NewGrafanaClient("http://192.168.16.127:3000","eyJrIjoicHA2aVJialBVcmQzU2V2dWp2ajJmZ0NQZUVoTVQwREgiLCJuIjoidGVzdCIsImlkIjoxfQ==")
-  data,_ := C.Get("/api/dashboards/uid/Y6vK4lwGz")
-  switch value := data.(type){
-  case map[string] string:
-    fmt.Println(value)
-  default:
-    fmt.Println("Default: ",value)
+  C,_ := NewGrafanaClient("http://192.168.16.127:3000","eyJrIjoiZExNdVNiR3VaamdHSkxmNnNWNDdORnY2bXEyODBMT1IiLCJuIjoidGVzdCIsImlkIjoxfQ==")
+//  data,err := C.Get("/api/dashboards/uid/CX9BS_wMk")
+//  data,err := C.Get("/api/org")
+  data,err := C.Get("/api/alerts")
+  if err != nil{
+    fmt.Println("client error: ",err)
   }
-}
+  switch value := (*data).(type){
+  case map[string]interface{}:
+    fmt.Println("map :",value)
+
+  case []interface{}:
+//    fmt.Println("slice: ",value)
+    for _,i := range(value){
+//      fmt.Println(k," : ",i)
+      if v,ok := i.(map[string]interface{});ok{
+//        fmt.Println(v["evalData"])
+        if k,ok := v["evalData"].(map[string]interface{});ok{
+          fmt.Println("K: ",k["evalMatches"])
+          if s,ok := k["evalMatches"].([]interface{});ok{
+                t := reflect.TypeOf(s[0])
+                fmt.Println(t.String())
+                fmt.Println("ok")
+                fmt.Println("s ",s)
+          }
+        }
+      }
+    }
+  default:
+    fmt.Println("default: ",value)
+  }
+}*/
