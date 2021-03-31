48c43e7677eae2e568dc2373eff72bfdee12452b
Bamboer <57797847+Bamboer@users.noreply.github.com>
2021-03-31 17:06:23 +0800

    Update httpclient.go

--- pkg/client/httpclient.go
+++ pkg/client/httpclient.go
@@ -1,55 +1,55 @@
 package client
 
 import (
-	"net/http"
-	"net/url"
-	"encoding/json"
+        "net/http"
+        "net/url"
+        "encoding/json"
 )
 
 type grafana_client struct {
-	uri        *url.URL
-	token      string
-	httpClient *http.Client
+        uri        *url.URL
+        token      string
+        httpClient *http.Client
 }
 
 func NewGrafanaClient(uri, token string) (*grafana_client, error) {
-	url, err := url.Parse(uri)
-	token = "Bearer " + token
+        url, err := url.Parse(uri)
+        token = "Bearer " + token
 //        info.Println(url)
 //        info.Println(token)
-	if err != nil {
-		info.Println(err)
-		return nil, err
-	}
-	return &grafana_client{
-		uri:        url,
-		token:      token,
-		httpClient: &http.Client{},
-	}, nil
+        if err != nil {
+                info.Println(err)
+                return nil, err
+        }
+        return &grafana_client{
+                uri:        url,
+                token:      token,
+                httpClient: &http.Client{},
+        }, nil
 }
 
 func (c *grafana_client) Get(path string, v interface{}) error {
-	uri := c.uri
-	uri.Path = path
-	req, err := http.NewRequest("GET", uri.String(), nil)
+        uri := c.uri
+        uri.Path = path
+        req, err := http.NewRequest("GET", uri.String(), nil)
         if err != nil {
-		info.Println(err)
-		return err
-	}
-	req.Header.Add("Authorization", c.token)
-	//        req.SetBasicAuth("admin", "admin")
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Accept", "application/json")
-	resp, err := c.httpClient.Do(req)
+                info.Println(err)
+                return err
+        }
+        req.Header.Add("Authorization", c.token)
+        //        req.SetBasicAuth("admin", "admin")
+        req.Header.Add("Content-Type", "application/json")
+        req.Header.Add("Accept", "application/json")
+        resp, err := c.httpClient.Do(req)
         if err != nil {
-		info.Println(err)
-		return err
-	}
-	defer resp.Body.Close()
-	if err = json.NewDecoder(resp.Body).Decode(v); err != nil {
-		info.Println(err)
-		return err
-	}
+                info.Println(err)
+                return err
+        }
+        defer resp.Body.Close()
+        if err = json.NewDecoder(resp.Body).Decode(v); err != nil {
+                info.Println(err)
+                return err
+        }
 //        log.Println(v)
-	return nil
+        return nil
 }
