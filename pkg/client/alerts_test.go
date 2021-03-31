aeca2c126adf95f745f948327823205a35989939
Bamboer <Bamboer@git.com>
2021-04-01 01:22:01 +0800

    add pkg/client/alerts_test.go

---
+++ pkg/client/alerts_test.go
@@ -0,0 +1,16 @@
+package alerts_test
+import (
+  "grafana/pkg/client"
+  "testing"
+)
+func TestAdd(t *testing.T){
+  ret = mymath.Add(2,3)
+  if ret != 5{
+   t.Error("Expected 5, got",ret)
+  }
+}
+func BenchmarkAdd(b *testing.B){
+  for i := 0; i< b.N; i++{
+    fmt.Sprintf("hello")
+  }
+}
