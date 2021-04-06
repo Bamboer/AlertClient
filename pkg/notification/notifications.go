2ead4ced2dabe1d1b7061138c47fc2b4577cfd25
Bamboer <57797847+Bamboer@users.noreply.github.com>
2021-04-06 18:09:12 +0800

    Update notifications.go

--- pkg/notification/notifications.go
+++ pkg/notification/notifications.go
@@ -1,19 +1,19 @@
 package notification
-import(
-  "io"
-  "os"
-  "log"
-  "flag"
-  "path"
-  "strings"
-  "grafana/pkg/client"
+
+import (
+        "gopkg.in/ini.v1"
+        "grafana/pkg/client"
+        "io"
+        "log"
+        "os"
+        "path"
+        "strings"
 )
 
-
 var (
-   info    *log.Logger
-   SENSOR  Notification
-   SNS   =  make(map[string] func(state string,msg client.SimpleInfo,b []byte)err )
+        info   *log.Logger
+//      SENSOR Notification
+        SNS    = make(map[string]func(state string, msg client.SimpleInfo, b []byte)error)
 )
 
 func init() {
@@ -23,20 +23,30 @@ func init() {
         if err != nil {
                 log.Println("Failed to open file: ", err)
         }
-        info = log.New(io.MultiWriter(os.Stdout, file), "Info: ", log.Ldate|log.Ltime|log.Lshortfile)
+        cfg, err := ini.Load(*ConfigFile)
+        if err != nil {
+                info.Println("Fail to read file: ", err)
+                os.Exit(1)
+        }
+        mode := cfg.Section("").Key("mode").In("dev", []string{"dev", "debug", "prd"})
+        if mode == "dev" || mode == "debug" {
+                info = log.New(io.MultiWriter(os.Stdout, file), "Info: ", log.Ldate|log.Ltime|log.Lshortfile)
+        } else if mode == "prd" {
+                info = log.New(file, "Info: ", log.Ldate|log.Ltime|log.Lshortfile)
+        }
 }
 
-type Notification interface{
-//send message to the receiver
-   Send(state string) error
+type Notification interface {
+        //send message to the receiver
+        Send(state string) error
 }
 
-func Emit(state string ,msg client.SimpleInfo,b []byte){
-   for k,send := range(SNS){
-     if err := send(state,msg,b);err !=nil{
-         info.Println(k,"send err: ",err)
-     }else{
-         info.Println(k,"send message: ")
-     }
-   }
+func Emit(state string, msg client.SimpleInfo, b []byte) {
+        for k, send := range SNS {
+                if err := send(state, msg, b); err != nil {
+                        info.Println(k, "send err: ", err)
+                } else {
+                        info.Println(k, "send message: ")
+                }
+        }
 }
