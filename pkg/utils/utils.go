525933d97bc0ca5c710c0113a1357bda4a21e5a9
Bamboer <57797847+Bamboer@users.noreply.github.com>
2021-04-06 18:10:58 +0800

    Create utils.go

---
+++ pkg/utils/utils.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+        "context"
+        "gopkg.in/ini.v1"
+        "io"
+        "log"
+        "os"
+        "path"
+        "strings"
+)
+
+var (
+        info  *log.Logger
+        UTILS = []func(ctx context.Context){}
+)
+
+func init() {
+        arg := path.Base(os.Args[0])
+        logfile := strings.ToLower(arg + ".log")
+        file, err := os.OpenFile(logfile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+        if err != nil {
+                log.Println("Failed to open file: ", err)
+        }
+        cfg, err := ini.Load(*ConfigFile)
+        if err != nil {
+                info.Println("Fail to read file: ", err)
+                os.Exit(1)
+        }
+        mode := cfg.Section("").Key("mode").In("dev", []string{"dev", "debug", "prd"})
+        if mode == "dev" || mode == "debug" {
+                info = log.New(io.MultiWriter(os.Stdout, file), "Info: ", log.Ldate|log.Ltime|log.Lshortfile)
+        } else if mode == "prd" {
+                info = log.New(file, "Info: ", log.Ldate|log.Ltime|log.Lshortfile)
+        }
+}
+
+func UtilRuner(ctx context.Context) {
+        for _, v := range UTILS {
+                go v(ctx)
+        }
+}
