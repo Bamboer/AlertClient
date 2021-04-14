c8b18de8d48fece261c19191a7eda3687c06611f
Bamboer <57797847+Bamboer@users.noreply.github.com>
2021-04-14 09:28:42 +0800

    Update dau.go

--- pkg/utils/dau.go
+++ pkg/utils/dau.go
@@ -1,32 +1,38 @@
 package utils
 
 import(
+  "os"
+  "fmt"
   "time"
   "context"
   "net/http"
   "net/url"
+  "net/smtp"
   "io/ioutil"
   "encoding/json"
   "path/filepath"
   "html/template"
-  "grafana/pkg/configer"
-  "grafana/pkg/notification"
   "github.com/aws/aws-sdk-go-v2/aws"
   "github.com/aws/aws-sdk-go-v2/config"
   "github.com/aws/aws-sdk-go-v2/service/cloudwatch"
   "github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
+  "grafana/pkg/configer"
+  "grafana/pkg/notification"
 )
 
+func init(){
+    UTILS = append(UTILS, DAU)
+}
 
 var(
   GraphiteURL = "http://10.50.24.197:7001/render"
   User = "8bf3584d"
   Pwd  = "6f30e011"
-  _,wk := time.Now().ISOWeek()
+  _,wk = time.Now().ISOWeek()
   DReport =  DailyReport{
          Timer: time.Now(),
          WK:    wk,
-    WeekDay: DayData{},
+         WeekDay: map[int]DayData{},
 }
 )
 
@@ -46,84 +52,31 @@ type DayData struct{
   Health  int
 }
 
-func init(){
-  UTILS = append(UTILS, DAU)
-}
-
 func DAU(ctx context.Context){
-  info.Println("start DAU job.")
-  for {
-    if time.Now().Hour() == 0{
-       if err := GImage();err != nil{
+        info.Println("Start DAU Reporter.")
+        if err := RenderDau();err != nil{
           info.Println(err)
-       }
-    }
-   select {
-   case <-ctx.Done():
-           info.Println("done")
-           return
-   default:
-   }
-    time.Sleep(1 *time.Hour)
-  }
+        }
+        for {
+            if time.Now().Hour() == 0{
+              if err := GImage();err != nil{
+                 info.Println(err)
+              }
+            }
+                select {
+                case <-ctx.Done():
+                        info.Println("done")
+                        return
+                default:
+                }
+                time.Sleep(1 * time.Hour)
+        }
 }
 
-func GImage()error{
-        //Generator time series for render image
-        t1 := int(time.Now().Unix()) * 1000
-        t2 := t1 - 86400000 * 7
 
-        grafana_conf := configer.ConfigParse()
-        uri, err := url.Parse(grafana_conf.Grafana_uri)
-        if err != nil {
-                info.Println("URL parse error: ", err)
-                return err
-        }
-        token := "Bearer " + grafana_conf.Grafana_token
-        uri.Path = "/render/d-solo/" + "000221" + "/" + "user-count"
-        c,_ := NewRender(grafana_conf.Grafana_uri,token)
-        req, err := http.NewRequest("GET", uri.String(), nil)
-        if err != nil {
-                info.Println("request generator error: ", err)
-                return err
-        }
-        // request header add
-        req.Header.Add("Authorization", token)
-        req.Header.Add("Content-Type", "application/json")
-        req.Header.Add("Accept", "application/json")
-        //add query data
-        q := req.URL.Query()
-        q.Add("orgid", strconv.Itoa(2))
-        q.Add("from", strconv.Itoa(t2))
-        q.Add("to", strconv.Itoa(t1))
-        q.Add("panelId", strconv.Itoa(15))
-        q.Add("width", "1000")
-        q.Add("height", "500")
-        q.Add("Var-FARM", "ec2-n66-svoice-cn-rel1")
-        req.URL.RawQuery = q.Encode()
-        info.Println("Render url: ", req.URL.String())
-        //request
-        resp, err := c.client.Do(req)
-        if err != nil {
-            return err
-        }
-        defer resp.Body.Close()
-        b, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            info.Println("iotuil Read error: ", err)
-            return err
-        }
-        if err := Mail(b);err != nil{
-            return err
-        }
-        return  nil
-}
-
-func Mail(b []byte)error{
-        boundary := "BamboerBoundary"
-        Header := make(map[string]string)
+func MailSend(b []byte)error{
+        var imgsrc string
         buffer := bytes.NewBuffer(nil)
-
         conf := configer.ConfigParse()
         notifications  := strings.Split(conf.Notifications,",")
         notifications_cc := strings.Split(conf.Notifications_cc,",")
@@ -139,36 +92,41 @@ func Mail(b []byte)error{
                 },
         }
         m, _ := notification.NewMail(conf.SmtpServer.Username, conf.SmtpServer.Password, conf.SmtpServer.SmtpAddress, conf.SmtpServer.Port)
-        Header["From"] = message.From
-        Header["To"] = strings.Join(message.To, ";")
-        Header["Cc"] = strings.Join(message.Cc, ";")
-        Header["Bcc"] = strings.Join(message.Bcc, ";")
-        Header["Subject"] = "SVoice Daily Report"
+        boundary := "BamboerBoundary"
+        Header := make(map[string]string)
+        Header["From"] = message.from
+        Header["To"] = strings.Join(message.to, ";")
+        Header["Cc"] = strings.Join(message.cc, ";")
+        Header["Bcc"] = strings.Join(message.bcc, ";")
+        Header["Subject"] = "SVoice  Daily Report"
         Header["Content-Type"] = "multipart/related;boundary=" + boundary
-        Header["Mime-Version"] = "1.0"
-        Header["Date"] = time.Now().UTC().String()
-        m.WriteHeader(buffer, Header)
+//        Header["Mime-Version"] = "1.0"
+//        Header["Date"] = time.Now().UTC().String()
 
-        if message.Attachment.WithFile {
+        m.writeHeader(buffer, Header)
+
+        if message.attachment.WithFile {
                 attachment := "\r\n--" + boundary + "\r\n"
                 attachment += "Content-Transfer-Encoding:base64\r\n"
                 //                attachment += "Content-Disposition:attachment\r\n"
-                attachment += "Content-Type:" + message.Attachment.ContentType + ";name=\"" + message.Attachment.Name + "\"\r\n"
-                attachment += "Content-ID: <" + message.Attachment.Name + "> \r\n\r\n"
-//                imgsrc = "<p><img src=\"cid:" + message.Attachment.Name + "\"></p>"
+                attachment += "Content-Type:" + message.attachment.ContentType + ";name=\"" + message.attachment.Name + "\"\r\n"
+                attachment += "Content-ID: <" + message.attachment.Name + "> \r\n\r\n"
+                imgsrc = "<p><img src=\"cid:" + message.attachment.Name + "\"></p>"
                 buffer.WriteString(attachment)
                 defer func() {
                         if err := recover(); err != nil {
                                 info.Println("Error: ", err)
                         }
                 }()
-                m.WriteFile(buffer, b)
+                m.writeFile(buffer, b)
         }
 
         body := "\r\n--" + boundary + "\r\n"
         body += "Content-Type: text/html; charset=UTF-8 \r\n"
         buffer.WriteString(body)
-        if err := GHtml(buffer);err != nil{
+//        body += renderMessage(state, imgsrc, msg)
+        err := GHtml(buffer)
+        if err != nil{
            return err
         }
 
@@ -179,43 +137,47 @@ func Mail(b []byte)error{
         return nil
 }
 
-func GHtml(b bytes.Buffer)error{
+func GHtml(b *bytes.Buffer)error{
   conf := configer.ConfigParse()
-  tpPath := conf.DauTpPath
   elb := conf.AWSELBName
   region := conf.AWSRegion
-  absPath,err := filepath.Abs(tpPath)
+  tpPath := conf.DauTpPath
+
+  access,err := Access(region,elb)
   if err != nil{
-     return err
-  }
-  tp,err := template.ParseFiles(absPath)
-  if err != nil{
-     return err
-  }
-  access,err := Access()
-  if err != nil{
-     return err
+    fmt.Println(err)
   }
   health,err := Health()
   if err != nil{
-     return err
+    fmt.Println(err)
   }
+
   t := time.Now()
-  td := t.Weekday()
-  tnow := time.Date(t.Year(),t.Month(),t.Day(),0,0,0,0,time.UTC).Unix()
-  DReport.WeekDay[int(td)] = DayData{Health: health[int(tnow])}
-  for i := 1;i <= int(td);i++{
-    t1 := tnow - 86400*i
-    td := int(time.Unix(t1,0).Weekday())
-    DReport.WeekDay[td] = DayData{Access: access[int(t1)] , Health: health[int(t1)]}
+  td := int(t.Weekday())
+  et := int(time.Date(t.Year(),t.Month(),t.Day(),0,0,0,0,time.Local).Unix())
+
+  DReport.WeekDay[td] = DayData{Health: health[et]*100 }
+  for i := 1; i <= int(td);i++{
+     t := time.Unix(int64(et - i*86400),0)
+     wk := int(t.Weekday())
+     DReport.WeekDay[wk] = DayData{Access: access[et - i*86400], Health: health[et - i*86400]*100}
   }
-  for i := int(td); i < 6; i++{
-    t1 := tnow + 86400 * (6 - i)
-    td := int(time.Unix(t1,0).Weekday())
-    DReport.WeekDay[td] = DayData{}
+  for i := int(td);i < 6; i++{
+   t := time.Unix(int64(et + (6-i)*86400),0)
+   wk := int(t.Weekday())
+   DReport.WeekDay[wk] = DayData{}
   }
-  if err := tp.Execute(b,TReport);err != nil{
-    return err
+
+  absPath,err := filepath.Abs(tpPath)
+  if err != nil{
+     fmt.Println(err)
+  }
+  tp,err := template.ParseFiles(absPath)
+  if err != nil{
+     fmt.Println(err)
+  }
+  if err := tp.Execute(b, DReport);err != nil{
+     return err
   }
   return nil
 }
@@ -227,7 +189,7 @@ func Access(region,elb string)(map[int]int,error){
   et := time.Date(t.Year(),t.Month(),t.Day(),0,0,0,0,time.Local)
   cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
   if err != nil {
-          info.Println("unable to load SDK config, %v", err)
+          fmt.Println("unable to load SDK config, %v", err)
           return data,err
   }
   client := cloudwatch.NewFromConfig(cfg)
@@ -247,7 +209,6 @@ func Access(region,elb string)(map[int]int,error){
   for _,v := range output.Datapoints{
     data[int((*v.Timestamp).Unix())] = int(*v.Sum)
   }
-  //info.Println(time.Unix(int64(1.617408e+09),0))
   return data,nil
 }
 
@@ -266,7 +227,6 @@ func Health()(map[int]int,error){
   q.Add("from","-144hours")
   q.Add("format","json")
   req.URL.RawQuery = q.Encode()
-//  info.Println("Render url: ", req.URL.String())
   resp, err := c.client.Do(req)
   if err != nil {
       return data,err
@@ -284,7 +244,6 @@ func Health()(map[int]int,error){
       data[int(v[1])] = int(v[0])
   }
   return data,nil
-//  t := time.Unix(int64(1.617408e+09),0)
 }
 
 
@@ -298,7 +257,7 @@ type Render struct{
 func NewRender(uri, user,password string)(*Render,error){
    url,err := url.Parse(uri)
    if err != nil{
-     info.Println("Error: ",err)
+     fmt.Println("Error: ",err)
      return nil,err
    }
    return &Render{
@@ -307,4 +266,54 @@ func NewRender(uri, user,password string)(*Render,error){
        password: password,
        client: &http.Client{},
    },nil
+}
+
+func RenderDau()error{
+        //Generator time series for render image
+        t1 := int(time.Now().Unix()) * 1000
+        t2 := t1 - 86400000*7
+
+        grafana_conf := configer.ConfigParse()
+        uri, err := url.Parse(grafana_conf.Grafana_uri)
+        if err != nil {
+                info.Println("URL parse error: ", err)
+        }
+        token := "Bearer " + grafana_conf.Grafana_token
+        uri.Path = "/render/d-solo/" + "000000221" + "/" + "user-count"
+        c,_ := NewRender(grafana_conf.Grafana_uri,token)
+        req, err := http.NewRequest("GET", uri.String(), nil)
+        if err != nil {
+                info.Println("request generator error: ", err)
+        }
+        // request header add
+        req.Header.Add("Authorization", token)
+        req.Header.Add("Content-Type", "application/json")
+        req.Header.Add("Accept", "application/json")
+        //add query data
+        q := req.URL.Query()
+        q.Add("orgid", strconv.Itoa(2))
+        q.Add("from", strconv.Itoa(t2))
+        q.Add("to", strconv.Itoa(t1))
+        q.Add("panelId", strconv.Itoa(15))
+        q.Add("var-FRAM", "ec2-cn-north-1-svoice-idg-rel")
+        q.Add("width", "1000")
+        q.Add("height", "500")
+        req.URL.RawQuery = q.Encode()
+        info.Println("Render url: ", req.URL.String())
+        //request
+        resp, err := c.client.Do(req)
+        if err != nil {
+                info.Println(err)
+        }
+        defer resp.Body.Close()
+        b, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+                info.Println("iotuil Read error: ", err)
+                return  err
+        }
+        if err := MailSend(b);err != nil{
+                info.Println(err)
+                return err
+        }
+        return  nil
 }
